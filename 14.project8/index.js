394a0517999938bf957e409e861eb7f80d986ee9
Jay singh <jaysingh7355@gmail.com>
2023-12-22 00:01:49 +0530

    todo app

---
+++ 14.project8/index.js
@@ -0,0 +1,50 @@
+console.log("Welcome to my todo app");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let GetTodoButton = document.getElementById('get-todos');
+// // registration of event listener
+// GetTodoButton.addEventListener("click",()=>{
+//     console.log("clicked");
+// })
